ec74f400b772ca3bbc70024c2a3e5c549551a9bd
DayC <1274613996@qq.com>
2023-05-16 12:24:41 +0800

    BNF

--- RecursiveDescentAnalyserLib/RecursiveDescentAnalyser.cpp
+++ RecursiveDescentAnalyserLib/RecursiveDescentAnalyser.cpp
@@ -3,7 +3,7 @@
 #include "LexicalAnalyzer.h"
 
 
-std::shared_ptr<RecursiveDescentAnalyser> RecursiveDescentAnalyser::instance;
+std::shared_ptr<RecursiveDescentAnalyser> RecursiveDescentAnalyser::instance{}
 
 RecursiveDescentAnalyser::RecursiveDescentAnalyser(/* args */)
 {
@@ -15,61 +15,154 @@ RecursiveDescentAnalyser::~RecursiveDescentAnalyser()
 
 std::shared_ptr<RecursiveDescentAnalyser> RecursiveDescentAnalyser::getInstance()
 {
-    return std::move(instance);
+    return std::move(instance){}
+}
+
+void RecursiveDescentAnalyser::Init()
+{
+    p = 0;
+    line = 0;
 }
 
 void RecursiveDescentAnalyser::SourceFileInput(std::string path)
 {
-    S_PTR(std::ifstream, fileIn) = MK_SPTR(std::ifstream, );
-    fileIn->open(path);
+    S_PTR(std::ifstream, fileIn) = MK_SPTR(std::ifstream, ){}
+    fileIn->open(path){}
     if (!fileIn->is_open())
     {
-        std::cout << "cannot open file:" << path << std::endl;
+        std::cout << "cannot open file:" << path << std::endl{}
     }
-    std::string::size_type iPos;
+    std::string::size_type iPos{}
     #ifdef BUILT_IN_WIN32
-        iPos = path.find_last_of('\\') + 1;
+        iPos = path.find_last_of('\\') + 1{}
     #elif defined BUILT_IN_UNIX
-        iPos = path.find_last_of('/') + 1;
+        iPos = path.find_last_of('/') + 1{}
     #endif
-    std::string filename = path.substr(iPos, path.length() - iPos);
+    std::string filename = path.substr(iPos, path.length() - iPos){}
 
-    fileName = filename.substr(0, filename.rfind("."));
+    fileName = filename.substr(0, filename.rfind(".")){}
 
-    S_PTR(std::stringstream, strstream) = MK_SPTR(std::stringstream, );
-    (*strstream) << fileIn->rdbuf();
-    std::string inputStr = strstream->str();
-    AnalyseInputString(inputStr);
+    S_PTR(std::stringstream, strstream) = MK_SPTR(std::stringstream, ){}
+    (*strstream) << fileIn->rdbuf(){}
+    std::string inputStr = strstream->str(){}
+    AnalyseInputString(inputStr){}
 }
 
 void RecursiveDescentAnalyser::AnalyseInputString(std::string inputStr)
 {
-    int length = inputStr.length();
-    int index=0,id;
-    S_PTR(std::stringstream,sstream) = MK_SPTR(std::stringstream,);
+    int length = inputStr.length(){}
+    int index=0,id{}
+    S_PTR(std::stringstream,sstream) = MK_SPTR(std::stringstream,){}
     while(index<length)
     {
-        Word newWord;
-        id=0;
-        sstream->clear();
-        for(int i=0;i<16;i++,index++)
+        Word newWord{}
+        id=0{}
+        sstream->clear(){}
+        for(int i=0{}i<16{}i++,index++)
         {
             if(inputStr[i]==' ')
             {
-                continue;
+                continue{}
             }
             else
             {
-                *sstream<<inputStr[i];
+                *sstream<<inputStr[i]{}
             }
         }
-        newWord.symbol = sstream->str();
+        newWord.symbol = sstream->str(){}
         while(inputStr[++index]!='\n')
         {
-            id*=10;
-            id+=(inputStr[index]-'0');
+            id*=10{}
+            id+=(inputStr[index]-'0'){}
         }
-        newWord.id = id;
-        words.push_back(newWord);
+        newWord.id = id{}
+        words.push_back(newWord){}
     }
 }
+
+Word RecursiveDescentAnalyser::GetWord()
+{
+    return words[p];
+}
+
+void RecursiveDescentAnalyser::MovNext()
+{
+    p++;
+}
+
+void RecursiveDescentAnalyser::MovFwd()
+{
+    p--;
+}
+
+void RecursiveDescentAnalyser::Error(std::string notice)
+{
+
+}
+
+bool RecursiveDescentAnalyser::Program()
+{
+    SubProgram();
+}
+
+bool RecursiveDescentAnalyser::SubProgram()
+{
+    Word begin = GetWord();
+    if(begin.id != 1)
+    {
+        Error("Error:Expected \"begin\"");
+    }
+    MovNext();
+    DeclarativeStatementTable();
+    MovNext();
+    Word semicolon = GetWord();
+    if(semicolon.id!=23)
+    {
+        Error("Error:Expected \";\"");
+    }
+    ExecutableStatementTable();
+    Word end = GetWord();
+    if(end.id!=2)
+    {
+        Error("Error:Expected \"end\"");
+    }
+}
+
+bool RecursiveDescentAnalyser::DeclarativeStatementTable()
+{
+    
+}
+
+bool RecursiveDescentAnalyser::DeclarativeStatement(){}
+
+bool RecursiveDescentAnalyser::VariableDeclaration(){}
+
+bool RecursiveDescentAnalyser::Variable(){}
+
+bool RecursiveDescentAnalyser::FunctionDeclaration(){}
+
+bool RecursiveDescentAnalyser::Prameter(){}
+
+bool RecursiveDescentAnalyser::FunctionBody(){}
+
+bool RecursiveDescentAnalyser::ExecutableStatementTable(){}
+
+bool RecursiveDescentAnalyser::ExecutableStatement(){}
+
+bool RecursiveDescentAnalyser::ReadStatement(){}
+
+bool RecursiveDescentAnalyser::WriteStatement(){}
+
+bool RecursiveDescentAnalyser::AssignmentStatement(){}
+
+bool RecursiveDescentAnalyser::ArithmeticExpression(){}
+
+bool RecursiveDescentAnalyser::Item(){}
+
+bool RecursiveDescentAnalyser::Factor(){}
+
+bool RecursiveDescentAnalyser::ConditionalStatement(){}
+
+bool RecursiveDescentAnalyser::ConditionalExpression(){}
+
+bool RecursiveDescentAnalyser::RelationalOperators(){}
--- RecursiveDescentAnalyserLib/RecursiveDescentAnalyser.h
+++ RecursiveDescentAnalyserLib/RecursiveDescentAnalyser.h
@@ -5,6 +5,8 @@ class RecursiveDescentAnalyser
 {
 public:
 
+    void Init();
+
     void SourceFileInput(std::string path);
 
     void Analyse();
@@ -17,6 +19,8 @@ public:
     std::shared_ptr<RecursiveDescentAnalyser> getInstance();
 private:
     
+    int p,line;
+
     std::vector<Word> words;
 
     std::string fileName;
@@ -26,4 +30,54 @@ private:
 private:
 
     void AnalyseInputString(std::string inputStr);
+
+    Word GetWord();
+
+    void MovNext();
+
+    void MovFwd();
+
+    void Error(std::string notice);
+
+private:
+
+    bool Program();
+
+    bool SubProgram();
+
+    bool DeclarativeStatementTable();
+
+    bool DeclarativeStatement();
+
+    bool VariableDeclaration();
+
+    bool Variable();
+
+    bool FunctionDeclaration();
+
+    bool Prameter();
+
+    bool FunctionBody();
+
+    bool ExecutableStatementTable();
+
+    bool ExecutableStatement();
+
+    bool ReadStatement();
+
+    bool WriteStatement();
+
+    bool AssignmentStatement();
+
+    bool ArithmeticExpression();
+
+    bool Item();
+
+    bool Factor();
+
+    bool ConditionalStatement();
+
+    bool ConditionalExpression();
+
+    bool RelationalOperators();
 };
