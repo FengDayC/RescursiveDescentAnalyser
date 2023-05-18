65451a42c01e0a0dfed184130efaafaf61da0dea
DayC <1274613996@qq.com>
2023-05-18 17:07:57 +0800

    BNF

--- RecursiveDescentAnalyserLib/RecursiveDescentAnalyser.cpp
+++ RecursiveDescentAnalyserLib/RecursiveDescentAnalyser.cpp
@@ -3,7 +3,7 @@
 #include "LexicalAnalyzer.h"
 
 
-std::shared_ptr<RecursiveDescentAnalyser> RecursiveDescentAnalyser::instance{}
+std::shared_ptr<RecursiveDescentAnalyser> RecursiveDescentAnalyser::instance;
 
 RecursiveDescentAnalyser::RecursiveDescentAnalyser(/* args */)
 {
@@ -15,7 +15,7 @@ RecursiveDescentAnalyser::~RecursiveDescentAnalyser()
 
 std::shared_ptr<RecursiveDescentAnalyser> RecursiveDescentAnalyser::getInstance()
 {
-    return std::move(instance){}
+    return std::move(instance);
 }
 
 void RecursiveDescentAnalyser::Init()
@@ -26,57 +26,57 @@ void RecursiveDescentAnalyser::Init()
 
 void RecursiveDescentAnalyser::SourceFileInput(std::string path)
 {
-    S_PTR(std::ifstream, fileIn) = MK_SPTR(std::ifstream, ){}
-    fileIn->open(path){}
+    S_PTR(std::ifstream, fileIn) = MK_SPTR(std::ifstream, );
+    fileIn->open(path);
     if (!fileIn->is_open())
     {
-        std::cout << "cannot open file:" << path << std::endl{}
+        std::cout << "cannot open file:" << path << std::endl;
     }
-    std::string::size_type iPos{}
+    std::string::size_type iPos;
     #ifdef BUILT_IN_WIN32
-        iPos = path.find_last_of('\\') + 1{}
+        iPos = path.find_last_of('\\') + 1;
     #elif defined BUILT_IN_UNIX
-        iPos = path.find_last_of('/') + 1{}
+        iPos = path.find_last_of('/') + 1;
     #endif
-    std::string filename = path.substr(iPos, path.length() - iPos){}
+    std::string filename = path.substr(iPos, path.length() - iPos);
 
-    fileName = filename.substr(0, filename.rfind(".")){}
+    fileName = filename.substr(0, filename.rfind("."));
 
-    S_PTR(std::stringstream, strstream) = MK_SPTR(std::stringstream, ){}
-    (*strstream) << fileIn->rdbuf(){}
-    std::string inputStr = strstream->str(){}
-    AnalyseInputString(inputStr){}
+    S_PTR(std::stringstream, strstream) = MK_SPTR(std::stringstream, );
+    (*strstream) << fileIn->rdbuf();
+    std::string inputStr = strstream->str();
+    AnalyseInputString(inputStr);
 }
 
 void RecursiveDescentAnalyser::AnalyseInputString(std::string inputStr)
 {
-    int length = inputStr.length(){}
-    int index=0,id{}
-    S_PTR(std::stringstream,sstream) = MK_SPTR(std::stringstream,){}
+    int length = inputStr.length();
+    int index=0,id;
+    S_PTR(std::stringstream,sstream) = MK_SPTR(std::stringstream,);
     while(index<length)
     {
-        Word newWord{}
-        id=0{}
-        sstream->clear(){}
-        for(int i=0{}i<16{}i++,index++)
+        Word newWord;
+        id=0;
+        sstream->clear();
+        for(int i=0;i<16;i++,index++)
         {
             if(inputStr[i]==' ')
             {
-                continue{}
+                continue;
             }
             else
             {
-                *sstream<<inputStr[i]{}
+                *sstream<<inputStr[i];
             }
         }
-        newWord.symbol = sstream->str(){}
+        newWord.symbol = sstream->str();
         while(inputStr[++index]!='\n')
         {
-            id*=10{}
-            id+=(inputStr[index]-'0'){}
+            id*=10;
+            id+=(inputStr[index]-'0');
         }
-        newWord.id = id{}
-        words.push_back(newWord){}
+        newWord.id = id;
+        words.push_back(newWord);
     }
 }
 
@@ -103,6 +103,7 @@ void RecursiveDescentAnalyser::Error(std::string notice)
 bool RecursiveDescentAnalyser::Program()
 {
     SubProgram();
+    return true;
 }
 
 bool RecursiveDescentAnalyser::SubProgram()
@@ -119,50 +120,105 @@ bool RecursiveDescentAnalyser::SubProgram()
     if(semicolon.id!=23)
     {
         Error("Error:Expected \";\"");
+        return false;
     }
     ExecutableStatementTable();
     Word end = GetWord();
     if(end.id!=2)
     {
         Error("Error:Expected \"end\"");
+        return false;
     }
+    return true;
 }
 
 bool RecursiveDescentAnalyser::DeclarativeStatementTable()
 {
     
+    return true;
 }
 
-bool RecursiveDescentAnalyser::DeclarativeStatement(){}
+bool RecursiveDescentAnalyser::DeclarativeStatement()
+{
+    return true;
+}
 
-bool RecursiveDescentAnalyser::VariableDeclaration(){}
+bool RecursiveDescentAnalyser::VariableDeclaration()
+{
+    return true;
+}
 
-bool RecursiveDescentAnalyser::Variable(){}
+bool RecursiveDescentAnalyser::Variable()
+{
+    return true;
+}
 
-bool RecursiveDescentAnalyser::FunctionDeclaration(){}
+bool RecursiveDescentAnalyser::FunctionDeclaration()
+{
+    return true;
+}
 
-bool RecursiveDescentAnalyser::Prameter(){}
+bool RecursiveDescentAnalyser::Prameter()
+{
+    return true;
+}
 
-bool RecursiveDescentAnalyser::FunctionBody(){}
+bool RecursiveDescentAnalyser::FunctionBody()
+{
+    return true;
+}
 
-bool RecursiveDescentAnalyser::ExecutableStatementTable(){}
+bool RecursiveDescentAnalyser::ExecutableStatementTable()
+{
+    return true;
+}
 
-bool RecursiveDescentAnalyser::ExecutableStatement(){}
+bool RecursiveDescentAnalyser::ExecutableStatement()
+{
+    return true;
+}
 
-bool RecursiveDescentAnalyser::ReadStatement(){}
+bool RecursiveDescentAnalyser::ReadStatement()
+{
+    return true;
+}
 
-bool RecursiveDescentAnalyser::WriteStatement(){}
+bool RecursiveDescentAnalyser::WriteStatement()
+{
+    return true;
+}
 
-bool RecursiveDescentAnalyser::AssignmentStatement(){}
+bool RecursiveDescentAnalyser::AssignmentStatement()
+{
+    return true;
+}
 
-bool RecursiveDescentAnalyser::ArithmeticExpression(){}
+bool RecursiveDescentAnalyser::ArithmeticExpression()
+{
+    return true;
+}
 
-bool RecursiveDescentAnalyser::Item(){}
+bool RecursiveDescentAnalyser::Item()
+{
+    return true;
+}
 
-bool RecursiveDescentAnalyser::Factor(){}
+bool RecursiveDescentAnalyser::Factor()
+{
+    return true;
+}
 
-bool RecursiveDescentAnalyser::ConditionalStatement(){}
+bool RecursiveDescentAnalyser::ConditionalStatement()
+{
+    return true;
+}
 
-bool RecursiveDescentAnalyser::ConditionalExpression(){}
+bool RecursiveDescentAnalyser::ConditionalExpression()
+{
+    return true;
+}
 
-bool RecursiveDescentAnalyser::RelationalOperators(){}
+bool RecursiveDescentAnalyser::RelationalOperators()
+{
+    return true;
+}
